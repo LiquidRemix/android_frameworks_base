f738dbcd3b8380cbb94dafa13f1c411e23a009c0
liquid0624 <rjbirnesser@gmail.com>
2018-04-01 18:45:24 -0400

    Revert "PackageManager: Catch possible NPE from unparsed platform package"
    This reverts commit 4ed9294660f638562841aa5a1e2ceb862266acb4.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9307,8 +9307,6 @@ public class PackageManagerService extends IPackageManager.Stub
             }
         } catch (PackageParserException e) {
             throw PackageManagerException.from(e);
-        } catch (NullPointerException e) {
-            throw new PackageManagerException(e.getMessage());
         } finally {
             Trace.traceEnd(TRACE_TAG_PACKAGE_MANAGER);
         }
