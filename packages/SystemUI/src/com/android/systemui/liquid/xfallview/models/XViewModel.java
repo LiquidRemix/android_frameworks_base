omnirom/android_frameworks_base_old

SystemUI: add handling of special features
Change-Id: I9a98748240a9c93bf1df63646e7ea63742d6236c

packages/SystemUI/src/com/android/systemui/omni/xfallview/models/XViewModel.java
@@ -1,0 +1,102 @@
+package com.android.systemui.omni.xFallView.models;
+
+
+import android.graphics.Bitmap;
+
+
+public class XViewModel {
+
+    private Bitmap bitmap;
+
+    private float posX;
+    private float posY;
+
+    private float pivotX;
+    private float pivotY;
+
+    private float speedY;
+
+    private float scale;
+
+    private int rotateAngle;
+
+    private int transparency;
+
+    public XViewModel(Bitmap bitmap, float posX, float posY, float pivotX, float pivotY, float speedY) {
+        this.bitmap = bitmap;
+        this.posX = posX;
+        this.posY = posY;
+        this.pivotX = pivotX;
+        this.pivotY = pivotY;
+        this.speedY = speedY;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosY(float posY) {
+        this.posY = posY;
+    }
+
+    public float getPivotX() {
+        return pivotX;
+    }
+
+    public void setPivotX(float pivotX) {
+        this.pivotX = pivotX;
+    }
+
+    public float getPivotY() {
+        return pivotY;
+    }
+
+    public void setPivotY(float pivotY) {
+        this.pivotY = pivotY;
+    }
+
+    public float getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(float speedY) {
+        this.speedY = speedY;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public int getRotateAngle() {
+        return rotateAngle;
+    }
+
+    public void setRotateAngle(int rotateAngle) {
+        this.rotateAngle = rotateAngle;
+    }
+
+    public int getTransparency() {
+        return transparency;
+    }
+
+    public void setTransparency(int transparency) {
+        this.transparency = transparency;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/omni/xfallview/utils/DisplayUtils.java
@@ -1,0 +1,19 @@
+package com.android.systemui.omni.xFallView.utils;
+
+import android.content.Context;
+
+
+public final class DisplayUtils {
+
+
+    private DisplayUtils() {
+    }
+
+
+    public static int dpToPx(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+
+        return (int) (dp * scale + .5f);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/omni/xfallview/utils/RandomUtil.java
@@ -1,0 +1,35 @@
+package com.android.systemui.omni.xFallView.utils;
+
+import java.util.Random;
+
+
+public final class RandomUtil {
+
+    private static final Random RANDOM = new Random();
+
+
+    private RandomUtil() {
+    }
+
+
+    public static int nextInt(int startInclusive) {
+        return RANDOM.nextInt(startInclusive);
+    }
+
+    public static int nextInt(int startInclusive, int endExclusive) {
+        return startInclusive == endExclusive
+                ? startInclusive
+                : startInclusive + RANDOM.nextInt(endExclusive - startInclusive);
+    }
+
+    public static float nextFloat(float startInclusive) {
+        return RANDOM.nextFloat() * startInclusive;
+    }
+
+    public static float nextFloat(float startInclusive, float endInclusive) {
+        return startInclusive == endInclusive
+                ? startInclusive
+                : startInclusive + (endInclusive - startInclusive) * RANDOM.nextFloat();
+    }
+
+}

packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -24,6 +24,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,7 +34,10 @@ import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
+import com.android.systemui.omni.OmniSystemUIUtils;
 import com.android.systemui.plugins.PluginPrefs;
+
+import java.util.Calendar;
 
 public class TunerFragment extends PreferenceFragment {
 
@@ -41,6 +46,10 @@ public class TunerFragment extends PreferenceFragment {
     private static final String KEY_BATTERY_PCT = "battery_pct";
     private static final String KEY_PLUGINS = "plugins";
     private static final CharSequence KEY_DOZE = "doze";
+    private static final String KEY_QS_SHOW_FUN = "qs_show_fun";
+    private static final String KEY_QS_FORCE_SHOW_FUN_TRIGGER = "qs_force_show_fun_trigger";
+    private static final String KEY_QS_FORCE_SHOW_FUN = "qs_force_show_fun";
+    private static final String KEY_QS_CATEGORY = "quick_settings";
 
     public static final String SETTING_SEEN_TUNER_WARNING = "seen_tuner_warning";
 
@@ -86,6 +95,29 @@ public class TunerFragment extends PreferenceFragment {
                 0) == 0) {
             if (getFragmentManager().findFragmentByTag(WARNING_TAG) == null) {
                 new TunerWarningFragment().show(getFragmentManager(), WARNING_TAG);
+            }
+        }
+
+        Preference preference = findPreference(KEY_QS_SHOW_FUN);
+        if (preference != null) {
+            if (!OmniSystemUIUtils.isXMasFunEnabled()) {
+                ((PreferenceScreen) findPreference(KEY_QS_CATEGORY)).removePreference(preference);
+            }
+        }
+        final Preference p = findPreference(KEY_QS_FORCE_SHOW_FUN_TRIGGER);
+        final Preference ts = findPreference(KEY_QS_FORCE_SHOW_FUN);
+        ts.setVisible(false);
+        if (p != null && ts != null) {
+            if (!OmniSystemUIUtils.isXMasFunEnabled()) {
+                p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        Log.d(TAG, "onPreferenceClick");
+                        p.setVisible(false);
+                        ts.setVisible(true);
+                        return false;
+                    }
+                });
             }
         }
     }

LiquidRemix/android_frameworks_base

packages/SystemUI/src/com/android/systemui/liquid/xfallview/models/XViewModel.java
@@ -1,0 +1,102 @@
+package com.android.systemui.liquid.xFallView.models;
+
+
+import android.graphics.Bitmap;
+
+
+public class XViewModel {
+
+    private Bitmap bitmap;
+
+    private float posX;
+    private float posY;
+
+    private float pivotX;
+    private float pivotY;
+
+    private float speedY;
+
+    private float scale;
+
+    private int rotateAngle;
+
+    private int transparency;
+
+    public XViewModel(Bitmap bitmap, float posX, float posY, float pivotX, float pivotY, float speedY) {
+        this.bitmap = bitmap;
+        this.posX = posX;
+        this.posY = posY;
+        this.pivotX = pivotX;
+        this.pivotY = pivotY;
+        this.speedY = speedY;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public float getPosX() {
+        return posX;
+    }
+
+    public void setPosX(float posX) {
+        this.posX = posX;
+    }
+
+    public float getPosY() {
+        return posY;
+    }
+
+    public void setPosY(float posY) {
+        this.posY = posY;
+    }
+
+    public float getPivotX() {
+        return pivotX;
+    }
+
+    public void setPivotX(float pivotX) {
+        this.pivotX = pivotX;
+    }
+
+    public float getPivotY() {
+        return pivotY;
+    }
+
+    public void setPivotY(float pivotY) {
+        this.pivotY = pivotY;
+    }
+
+    public float getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(float speedY) {
+        this.speedY = speedY;
+    }
+
+    public float getScale() {
+        return scale;
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public int getRotateAngle() {
+        return rotateAngle;
+    }
+
+    public void setRotateAngle(int rotateAngle) {
+        this.rotateAngle = rotateAngle;
+    }
+
+    public int getTransparency() {
+        return transparency;
+    }
+
+    public void setTransparency(int transparency) {
+        this.transparency = transparency;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/liquid/xfallview/utils/DisplayUtils.java
@@ -1,0 +1,19 @@
+package com.android.systemui.liquid.xFallView.utils;
+
+import android.content.Context;
+
+
+public final class DisplayUtils {
+
+
+    private DisplayUtils() {
+    }
+
+
+    public static int dpToPx(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+
+        return (int) (dp * scale + .5f);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/liquid/xfallview/utils/RandomUtil.java
@@ -1,0 +1,35 @@
+package com.android.systemui.liquid.xFallView.utils;
+
+import java.util.Random;
+
+
+public final class RandomUtil {
+
+    private static final Random RANDOM = new Random();
+
+
+    private RandomUtil() {
+    }
+
+
+    public static int nextInt(int startInclusive) {
+        return RANDOM.nextInt(startInclusive);
+    }
+
+    public static int nextInt(int startInclusive, int endExclusive) {
+        return startInclusive == endExclusive
+                ? startInclusive
+                : startInclusive + RANDOM.nextInt(endExclusive - startInclusive);
+    }
+
+    public static float nextFloat(float startInclusive) {
+        return RANDOM.nextFloat() * startInclusive;
+    }
+
+    public static float nextFloat(float startInclusive, float endInclusive) {
+        return startInclusive == endInclusive
+                ? startInclusive
+                : startInclusive + (endInclusive - startInclusive) * RANDOM.nextFloat();
+    }
+
+}

packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -24,6 +24,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,7 +34,10 @@ import com.android.internal.hardware.AmbientDisplayConfiguration;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.R;
+import com.android.systemui.liquid.SystemUIUtils;
 import com.android.systemui.plugins.PluginPrefs;
+
+import java.util.Calendar;
 
 public class TunerFragment extends PreferenceFragment {
 
@@ -40,6 +45,10 @@ public class TunerFragment extends PreferenceFragment {
 
     private static final String KEY_PLUGINS = "plugins";
     private static final CharSequence KEY_DOZE = "doze";
+    private static final String KEY_QS_SHOW_FUN = "qs_show_fun";
+    private static final String KEY_QS_FORCE_SHOW_FUN_TRIGGER = "qs_force_show_fun_trigger";
+    private static final String KEY_QS_FORCE_SHOW_FUN = "qs_force_show_fun";
+    private static final String KEY_QS_CATEGORY = "quick_settings";
 
     public static final String SETTING_SEEN_TUNER_WARNING = "seen_tuner_warning";
 
@@ -85,6 +94,31 @@ public class TunerFragment extends PreferenceFragment {
                 0) == 0) {
             if (getFragmentManager().findFragmentByTag(WARNING_TAG) == null) {
                 new TunerWarningFragment().show(getFragmentManager(), WARNING_TAG);
+            }
+        }
+
+        Preference preference = findPreference(KEY_QS_SHOW_FUN);
+        if (preference != null) {
+            if (!SystemUIUtils.isXMasFunEnabled()) {
+                ((PreferenceScreen) findPreference(KEY_QS_CATEGORY)).removePreference(preference);
+            }
+        }
+        final Preference p = findPreference(KEY_QS_FORCE_SHOW_FUN_TRIGGER);
+        final Preference ts = findPreference(KEY_QS_FORCE_SHOW_FUN);
+        ts.setVisible(false);
+        if (p != null && ts != null) {
+            if (!SystemUIUtils.isXMasFunEnabled()) {
+                p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        Log.d(TAG, "onPreferenceClick");
+                        p.setVisible(false);
+                        ts.setVisible(true);
+                        return false;
+                    }
+                });
+            } else {
+                p.setVisible(false);
             }
         }
     }