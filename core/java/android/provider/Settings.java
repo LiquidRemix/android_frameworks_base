fd27526cb6532242165eb2e46528b3885b0a39b0
Pranav Vashi <neobuddy89@gmail.com>
2019-10-27 21:30:29 +0530

    Base: Add battery styles and customizations for Android 10 [1/2]
    @neobuddy89:
    * Handle QS battery modes
    * Convert ThemedBatteryDrawable from kotlin to java with extra goodies of setting percentage.
    * Treat ThemedBatteryDrawable and BatteryMeterDrawableBase separately.
    * Forward port styles by Ian and Danesh & improvements by LOS team.
    
    @SKULSHADY:
    - Convert to SettingsObserver
    - Convert ThemedBatteryDrawable back to kotlin
    - Integrate show % while charging into % settings
    - Remove show % inside icon
    - Remove QS battery mode
    - Fix Circle icon size
    - Cleanup tuner code
    
    Change-Id: I666296d81bf0dde69c811d7f98d2989291f7a8b6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4480,10 +4480,7 @@ public final class Settings {
          *    1 - Show percentage
          * @hide
          */
-        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-
-        /** @hide */
-        private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = BOOLEAN_VALIDATOR;
+        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
 
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
@@ -4630,6 +4627,21 @@ public final class Settings {
                 ANY_STRING_VALIDATOR;
 
         /**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+          * Statusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage
+          * 2: Display the battery percentage only while charging
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4687,7 +4699,6 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
-            SHOW_BATTERY_PERCENT,
             NOTIFICATION_VIBRATION_INTENSITY,
             RING_VIBRATION_INTENSITY,
             HAPTIC_FEEDBACK_INTENSITY,
@@ -4818,7 +4829,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
-            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO);
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER);
@@ -4918,7 +4928,6 @@ public final class Settings {
             VALIDATORS.put(WIFI_STATIC_NETMASK, WIFI_STATIC_NETMASK_VALIDATOR);
             VALIDATORS.put(WIFI_STATIC_DNS1, WIFI_STATIC_DNS1_VALIDATOR);
             VALIDATORS.put(WIFI_STATIC_DNS2, WIFI_STATIC_DNS2_VALIDATOR);
-            VALIDATORS.put(SHOW_BATTERY_PERCENT, SHOW_BATTERY_PERCENT_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_BATTERY_INFO, LOCKSCREEN_BATTERY_INFO_VALIDATOR);
             VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER,
